4fa7c29f47027e5cb8061114970c97a7daa87cae
Karabo Rithuri <rithurikarabo@gmail.com>
2023-07-27 12:31:19 +0200

    function to store code, for readability

--- checker.py
+++ checker.py
@@ -1,34 +1,35 @@
-import string
+def password_checker(password):
+    import string
 
-special = list(string.punctuation)
-characters = 0
-digits = 0
-upper = 0
-lower = 0
+    special = list(string.punctuation)
+    characters = 0
+    digits = 0
+    upper = 0
+    lower = 0
 
-password = input("Enter password: ")
+    password = input("Enter password: ")
 
-if(len(password)<8):
-    print("Password should be 8 characters!")
-elif(len(password)>8):
-    for char in password:
-        if char.isdigit():
-            digits += 1
-        elif char.isalpha():
-            if char.isupper():
-                upper += 1
-            elif char.islower():
-                lower +=1
-        elif char in special:
-            characters += 1
+    if(len(password)<8):
+        print("Password should be 8 characters!")
+    elif(len(password)>8):
+        for char in password:
+            if char.isdigit():
+                digits += 1
+            elif char.isalpha():
+                if char.isupper():
+                    upper += 1
+                elif char.islower():
+                    lower +=1
+            elif char in special:
+                characters += 1
 
-if(upper <=0):
-    print("Invalid password! Must contain uppercase!")
-elif(lower <=0):
-    print("Invalid password! Must contain lowercase!")
-elif(characters <=0):
-    print("Invalid password! Must contain special characters!")
-elif(digits <=0):
-    print("Invalid password! Must contain digits!")
-else:
-    print("Password does not meet requirements!")
+    if(upper <=0):
+        print("Invalid password! Must contain uppercase!")
+    elif(lower <=0):
+        print("Invalid password! Must contain lowercase!")
+    elif(characters <=0):
+        print("Invalid password! Must contain special characters!")
+    elif(digits <=0):
+        print("Invalid password! Must contain digits!")
+    else:
+        print("Password does not meet requirements!")
