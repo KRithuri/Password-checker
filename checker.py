65feb48cbb3110eb3dd1a050d3e524de14ef775a
Karabo Rithuri <rithurikarabo@gmail.com>
2023-07-27 12:33:35 +0200

    code to execute if password meets requirements

--- checker.py
+++ checker.py
@@ -29,6 +29,8 @@ def password_checker(password):
         print("Invalid password! Must contain special characters!")
     elif(digits <=0):
         print("Invalid password! Must contain digits!")
+    elif (len(password) > 8 and upper > 0 and lower > 0 and characters > 0 and digits > 0 and (password.endswith("@gmail.com") or password.endswith("@outlook.com") or password.endswith("yahoo.com"))):
+        print("Password meets requirements!")
     else:
         print("Password does not meet requirements!")
 
